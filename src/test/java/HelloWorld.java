3ba22ba9fce40d6d181306b6c66fc3ff6c71c973
hpracjos <pracs.j06@gmail.com>
2021-01-12 19:42:51 +0000

    UpdatedbyLearnGIT

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Updating HelloWorldJavaFile");
+        System.out.println("LearnGIT added update");
     }
 }
